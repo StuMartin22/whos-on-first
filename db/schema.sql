a91ac4a955352afb52f9265e2567b53d99cc0a4a
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-04-26 15:52:13 -0500

    fixed keys in schema, added data for employees, created employee table.

--- db/schema.sql
+++ db/schema.sql
@@ -18,9 +18,9 @@ FOREIGN KEY (department_id) REFERENCES department(id)
 CREATE TABLE employee_info(
 id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
-last_name: VARCHAR(30),
-role_id INT not NULL,
-FOREIGN KEY (role_id) REFERENCES personnel_role(id)
-manager_id:INT NULL,
+last_name VARCHAR(30),
+role_id INT NOT NULL,
+FOREIGN KEY (role_id) REFERENCES personnel_role(id),
+manager_id INT,
 FOREIGN KEY (manager_id) REFERENCES employee_info(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,8 +4,7 @@ INSERT INTO department(department_name) VALUES ('Sales'),
 ('Pretend Management'),
 ('Human Resources');
 
-
-INSERT INTO personnel_role(title, salary, locale_id)
+INSERT INTO personnel_role(title, salary, department_id)
 VALUES ('Sales', '75000', 4),
 ('Head Honcho', '250000', 1),
 ('Middle Management', '100000', 2),
@@ -13,4 +12,10 @@ VALUES ('Sales', '75000', 4),
 ('HR', '75000', 5);
 
 INSERT INTO employee_info(id, first_name, last_name, role_id, manager_id)
-VALUES
+VALUES (1, 'Mike', 'Honcho', 1, NULL),
+(2, 'Michael', 'Scott', 2, 1),
+(3, 'Dwight', 'Schrute', 3, 2),
+(4, 'Jim', 'Halpert', 4, 2),
+(5, 'Pam', 'Halpert', 4, 2),
+(6, 'Holly', 'Flax' 5, 2),
+(7, 'Toby', 'Flenderson', 5, 2);
